5e7c98138f94a3e5a5736ceb27c943bd384532e4
unknown <Dunyafm@code.edu.az>
2022-01-31 13:20:06 -0800

    Js-arr.find.push

--- Js-arr.find.push/assets/js/javascript.js
+++ Js-arr.find.push/assets/js/javascript.js
@@ -1,3 +1,48 @@
+//Task
+
+class arrayclass {
+
+    constructor() {
+        this.newArr = [];
+
+
+    }
+
+    getFind(arr, callback) {
+        let result=0;
+        for (const item of arr) {
+            if (callback(item)) {
+                result=item
+                
+            }
+            console.log(result);
+        }
+        
+       
+    }
+    getAdd(arr) {
+        return this.newArr.push(arr);
+    }
+
+
+
+}
+let arrr = [10,15,20,25,30]
+let array = new arrayclass();
+array.getAdd(5)
+array.getAdd(10);
+array.getAdd(5)
+console.log(array);
+
+let isnum = function(n){
+        return n > 15
+}
+array.getFind(arrr,isnum)
+
+
+
+//praktika
+
 let foods = ["banan,tomatoes,cheeseburger,cheese,corots,rice,braed,milk,apple,beef"];
 function isFruits(foods) {
     return foods.name == "banan,apple";
